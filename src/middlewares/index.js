05c9a00593fd5b1e08bb5d26cd6f90662a26d2df
Poushali14devv <poushalimajumder141102@gmail.com>
2026-04-30 13:23:35 +0530

    added middlewares for auth

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,2 +1,9 @@
+// module.exports = {
+//     AuthRequestMiddlewares:require('./auth-request-middlewares')
+
+// }
+
+
 module.exports = {
+    AuthRequestMiddlewares: require('./auth-request-middlewares')
 }
