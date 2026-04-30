a4a63e967ddadd8c31931d3354241dd7b44906ec
Poushali14devv <poushalimajumder141102@gmail.com>
2026-04-30 15:29:17 +0530

    added disauthenticated middleware

--- src/utils/common/auth.js
+++ src/utils/common/auth.js
@@ -1,3 +1,33 @@
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+
+// const { ServerConfig } = require('../../config');
+// function checkPassword(plainPassword, encryptedPassword) {
+//     try {
+//         return bcrypt.compareSync(plainPassword, encryptedPassword);
+//     } catch(error) {
+//         console.log(error);
+//         throw error;
+//     }
+// }
+
+// function createToken(input) {
+//     try {
+//         return jwt.sign(input, ServerConfig.JWT_SECRET, {expiresIn: ServerConfig.JWT_EXPIRY});
+//     } catch(error) {
+//         console.log(error);
+//         throw error;
+//     }
+// }
+
+// module.exports = {
+//     checkPassword,
+//     createToken
+// }
+
+
+
+
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
@@ -20,7 +50,17 @@ function createToken(input) {
     }
 }
 
+function verifyToken(token) {
+    try {
+        return jwt.verify(token, ServerConfig.JWT_SECRET);
+    } catch(error) {
+        console.log(error);
+        throw error;
+    }
+}
+
 module.exports = {
     checkPassword,
-    createToken
+    createToken,
+    verifyToken
 }
