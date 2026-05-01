8db5dc15626da0c45c7c0067d137f5df97b0621e
Poushali14devv <poushalimajumder141102@gmail.com>
2026-05-01 20:59:48 +0530

    added default customer role while signup

--- src/models/user_role.js
+++ src/models/user_role.js
@@ -14,11 +14,11 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User_Role.init({
-    user_id: {
+    userId: {
       type: DataTypes.INTEGER,
       allowNull: false
     },
-    role_id: {
+    roleId: {
       type: DataTypes.INTEGER,
       allowNull: false
     }
@@ -28,3 +28,35 @@ module.exports = (sequelize, DataTypes) => {
   });
   return User_Role;
 };
+
+
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class User_Role extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   }
+//   User_Role.init({
+//     userId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false
+//     },
+//     roleId: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false
+//     }
+//   }, {
+//     sequelize,
+//     modelName: 'User_Role',
+//   });
+//   return User_Role;
+// };
